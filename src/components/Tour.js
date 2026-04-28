2c51aecdb4f029e7e4f583fb5f624baf95608c36
ranjith1807 <v.ranjith9095490240@gmail.com>
2026-04-28 21:53:35 +0530

    solved

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -12,8 +12,7 @@ const Tour = ({ id, image, info, name, price, removeTour }) => {
           <h4 className="tour-price">${price}</h4>
         </div>
         <p>
-          {/* Displays either full text or limits to 200 characters */}
-          {readMore ? info : `${info.substring(0, 200)}... `}
+          {readMore ? info : `${info.substring(0, 200)}...`}
           <button onClick={() => setReadMore(!readMore)}>
             {readMore ? 'See Less' : 'Show More'}
           </button>
